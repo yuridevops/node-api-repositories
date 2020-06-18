496c3564e3c5ecc449b66ca9dfd2c82ed6ec9d45
yuridevops <yuri.devops@hmail.com>
2020-06-17 21:09:59 -0300

    first commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid } = require("uuidv4")
 
 // const { uuid } = require("uuidv4");
 
@@ -11,23 +12,48 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+  console.log(request.body)
+  const repositorie = { id: uuid(), title, url, techs, likes: 0 }
+
+  repositories.push(repositorie)
+
+  return response.status(200).json(repositorie)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+  const repoIndex = repositories.findIndex(repo => repo.id === request.params.id)
+  console.log(repoIndex)
+  if (repoIndex > 0) {
+    repositories[repoIndex] = { ...repositories[repoIndex], title, url, techs }
+    return response.status(200).json(repositories[repoIndex])
+  }
+  return response.status(400).json({ message: 'Repositorie not found' })
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const repoIndex = repositories.findIndex(repo => repo.id === request.params.id)
+  if (repoIndex >= 0) {
+    repositories.splice(repoIndex, 1)
+    return response.status(204).json({message: "Deleted with success"})
+  }
+  return response.status(400).json({ message: 'Repositorie not found' })
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  console.log(request.params.id)
+  const repoIndex = repositories.findIndex(repo => repo.id === request.params.id)
+  console.log(repoIndex)
+  if (repoIndex >= 0) {
+    repositories[repoIndex].likes += 1
+    return response.status(200).json(repositories[repoIndex])
+  }
+  return response.status(400).json({ message: 'Repositorie not found' })
 });
 
 module.exports = app;
